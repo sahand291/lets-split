98c9909b282fc91e3b16f16592f3b4c4357a8b00
Sahand <sahandkarimkhani@gmail.com>
2025-08-19 23:40:04 +0330

    chore(core): add eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <article className="text-center">
-          <h1 className="text-6xl mb-8">
-            ⌨️🪓 Let's Split
-          </h1>
+          <h1 className="text-6xl mb-8">⌨️🪓 Let&apos;s Split</h1>
           <span className="text-gray-300">Coming Soon...</span>
         </article>
       </main>
